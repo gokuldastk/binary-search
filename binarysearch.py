8cdee0c6a040c6465362345d8230a509eb8ec64f
gokuldastk <150172788+gokuldastk@users.noreply.github.com>
2024-06-14 16:17:12 +0530

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,5 +1,3 @@
-Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> def binary_search(ar, target):
 ...     
 ...     left=0
@@ -8,10 +6,10 @@ Type "help", "copyright", "credits" or "license()" for more information.
 ...     while left <= right:
 ...         mid = left + (right - left) // 2
 ...         
-...         if arr[mid] == target:
+...         if ar[mid] == target:
 ...             return mid
 ...         
-...         elif arr[mid] < target:
+...         elif ar[mid] < target:
 ...             left = mid + 1
 ...             
 ...         else:
@@ -21,9 +19,9 @@ Type "help", "copyright", "credits" or "license()" for more information.
 ... 
 ... 
 ... ar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-... target = 7
+... target = 3
 ... 
-... result = binary_search(arr, target)
+... result = binary_search(ar, target)
 ... if result != -1:
 ...     print("Element found at index",str(ar))
 ... else:
